93b4df92c1a169b7ea645b3fc53fa208bd275f28
Bastien Prompsy <bastienprompsy@gmail.com>
2025-05-07 22:25:07 +0200

    correction url front

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,7 +1,7 @@
 // front-end/src/services/api.js
 import axios from 'axios';
 
-const API_URL = process.env.VUE_APP_API_URL || 'http://localhost:3000/api';
+const API_URL = process.env.VUE_APP_API_URL;
 
 // Création de l'instance axios avec configuration de base
 const apiClient = axios.create({
